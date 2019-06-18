78aab75145cce3fe9aec02b69d8f26e70b7337f4
pankaj_rupapara <pankaj@thinkbiz.co.in>
2019-06-18 17:52:29 +0530

    add commas for price

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-// myNumber.value = commify(myNumber.value)
-// myNumber.addEventListener("change", function(){
-//   commify(event.target.value)
-// })
-
-function commify(value){
-  var chars = value.split("").reverse()
-  var withCommas = []
-  for(var i = 1; i <= chars.length; i++ ){
-    withCommas.push(chars[i-1])
-    if(i%3==0 && i != chars.length ){  
-      withCommas.push(",")
-    }
-  }
-  var val = withCommas.reverse().join("")
-  myNumber.parentNode.setAttribute("comma-value",val)
-}
+var withCommas = Number(document.getElementById('number').innerHTML).toLocaleString('en');
+document.getElementById('number').innerHTML = withCommas;
